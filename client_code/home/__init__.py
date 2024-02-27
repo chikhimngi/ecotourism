25f27e041052b6e6afea1e41e6fea9ee286ba252
chikhaoui.mongi@gmail.com <chikhaoui.mongi@gmail.com>
2024-02-27 19:55:31 +0000

    Created kef; Edited home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import homeTemplate
 from anvil import *
+from ..kef import kef
 
 class home(homeTemplate):
   def __init__(self, **properties):
@@ -7,5 +8,10 @@ class home(homeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def vist_kef_click(self, **event_args):
+    self.column_panel01.clear()
+    self.content_panel01.add_component(kef())
+
 
 
